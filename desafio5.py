aec5ffe397dc0dc770d6fcb6514e40d99d89be9d
Eduarda Timóteo <eduarda.silva7739@gmail.com>
2025-05-19 14:39:36 -0300

    Add files via upload

---
+++ desafio5.py
@@ -0,0 +1,11 @@
+n1 = int(input('Digite um valor: '))
+so = n1 + 1
+su = n1 - 1
+
+print(f'O sucessor de {n1} é {so} e o seu antecessor é {su}' )
+
+
+num = int (input ("Me fale um número inteiro qualquer:"))
+
+print (f"O sucessor de {num} é" , (num + 1))
+print (f"O antecessor de {num} é" , (num - 1)) 
